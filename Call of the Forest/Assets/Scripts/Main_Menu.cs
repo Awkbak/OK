1b0cd98498d9f917a3844455002468f69e6b86a2
Kirsti <camlin@carolina.rr.com>
2015-12-15 15:26:00 -0500

    my stuff...

---
+++ Call of the Forest/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Main_Menu : MonoBehaviour {
+
+    public Slider diff;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Game_Environment.setDifficulty(diff.value);
+	}
+
+    //Change to the game
+    public void changeToGame()
+    {
+        Application.LoadLevel("Game");
+    }
+
+    //Change to Instructions screen
+    public void changeToInstructions()
+    {
+        Application.LoadLevel("Instructions");
+    }
+}
